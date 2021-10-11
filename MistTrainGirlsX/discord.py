0ca00785237c6bae9de4f227416c234a861bae93
ken1882 <qj6w94g0q@gmail.com>
2021-10-11 22:44:09 +0800

    fix config parser case bug

--- MistTrainGirlsX/discord.py
+++ MistTrainGirlsX/discord.py
@@ -7,7 +7,8 @@ def update_player_profile(name, level):
   if not ConfigFilePath:
     return
   config = configparser.ConfigParser()
+  config.optionxform = str
   config.read(ConfigFilePath)
-  config['SmallImageTooltip'] = f"{name} - Rank.{level}"
+  config['Images']['SmallImageTooltip'] = f"{name} - Rank.{level}"
   with open(ConfigFilePath, 'w') as fp:
     config.write(fp)
